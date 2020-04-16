cf995f4af71fc7b7ae0d1c25851f59b3ec10bb6a
Yifei Wang <yifei.wang828@duke.edu>
2020-04-15 20:45:42 -0400

    do some cleaning

--- codes/diff_csv.sh
+++
@@ -1,5 +0,0 @@
-
-diff ./SkidSteer_2019-08_clean_train.csv ./image2price/SkidSteer_2019-08_clean_train.csv
-diff ./SkidSteer_2019-08_clean_val.csv ./image2price/SkidSteer_2019-08_clean_val.csv
-
-echo "Done"
